bd97eff6b4b2ee9d67b68bf60e1d692a261682db
yilles <shuwei73.mail@gmail.com>
2024-05-09 15:38:38 +0800

    update

--- two_sum/c/submit2.c
+++ two_sum/c/submit2.c
@@ -3,8 +3,8 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 typedef struct {
-  int key;
-  int value;
+    int key;
+    int value;
 } dictionary;
 
 dictionary* hashTable;
@@ -12,50 +12,50 @@ dictionary* hashTable;
 int mod = 701;
 //Hash Function
 int h(int key) {
-  int result = key % mod;
-  if (result < 0)
-    result = 0 - result;
-  return result;
+    int result = key % mod;
+    if (result < 0)
+        result = 0 - result;
+    return result;
 }
 
 dictionary search(int key) {
-  int index = h(key);
-  return hashTable[index];
+    int index = h(key);
+    return hashTable[index];
 }
 
 void insert(int key, int value) {
-  int index = h(key);
-  hashTable[index].key = key;
-  hashTable[index].value = value;
+    int index = h(key);
+    hashTable[index].key = key;
+    hashTable[index].value = value;
 }
 
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-  int* result = (int*)malloc(sizeof(int)*2);
-  hashTable = (dictionary*)malloc(sizeof(dictionary)*mod);
-  for (int i = 0; i < numsSize; ++i) {
-    if (search(target - nums[i]).key + nums[i] != target) {
-      int key = nums[i];
-      int value = i;
-      insert(key, value);
+    int* result = (int*)malloc(sizeof(int)*2);
+    hashTable = (dictionary*)malloc(sizeof(dictionary)*mod);
+    for (int i = 0; i < numsSize; ++i) {
+        if (search(target - nums[i]).key + nums[i] != target) {
+            int key = nums[i];
+            int value = i;
+            insert(key, value);
+        }
+        else {
+            *returnSize = 2;
+            result[0] = search(target - nums[i]).value;
+            result[1] = i;
+            free(hashTable);
+            return result;
+        }
     }
-    else {
-      *returnSize = 2;
-      result[0] = search(target - nums[i]).value;
-      result[1] = i;
-      free(hashTable);
-      return result;
-    }
-  }
-  free(hashTable);
-  return result;
+    free(hashTable);
+    return result;
 }
 
 void safeFree(void** malloced_int_pointer) {
-  //malloced_int_pointer != NULL is to avoid: int* result = NULL; safeFree(&result);
-  //*malloced_int_pointer != NULL is to avoid free twice and more: safeFree(&result); safeFree(&result);
-  //Don't avoid: int* result; safeFree(&result); Please let the pointer initialize to NULL.
-  if (malloced_int_pointer != NULL && *malloced_int_pointer != NULL) {
-    free(*malloced_int_pointer);
-    *malloced_int_pointer = NULL;
-  }
+    //malloced_int_pointer != NULL is to avoid: int* result = NULL; safeFree(&result);
+    //*malloced_int_pointer != NULL is to avoid free twice and more: safeFree(&result); safeFree(&result);
+    //Don't avoid: int* result; safeFree(&result); Please let the pointer initialize to NULL.
+    if (malloced_int_pointer != NULL && *malloced_int_pointer != NULL) {
+      free(*malloced_int_pointer);
+      *malloced_int_pointer = NULL;
+    }
 }
--- two_sum/c/test.c
+++ two_sum/c/test.c
@@ -2,41 +2,44 @@
 #include <time.h>
 #include "common.h"
 
-void funcTimer(int* msec, int** result, int* nums, int numsSize, int target, int* returnSize, int*(*func)(int*, int, int, int*)){
-    clock_t init_time = clock();
+void funcTimer(long* time, int** result, int* nums, int numsSize, int target, int* returnSize, int*(*func)(int*, int, int, int*)) {
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
     *result = (*func)(nums, numsSize, target, returnSize);
-    clock_t end_time = clock();
-    if (msec == NULL)
-      *msec = 0;
-    *msec += (end_time - init_time) * 1000 / CLOCKS_PER_SEC;
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    if (time == NULL)
+        *time = 0;
+    *time += (end.tv_sec - start.tv_sec) * 1e9 + (end.tv_nsec - start.tv_nsec);
     return;
 }
 
-int main(){
-  clock_t init_time, end_time;
-  int msec = 0;
-  int nums[] = {5, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 3, -3};
-  int numsSize = 40;
-  int target = 0;
-  int returnSize = 0;
-  int testCounts = 10000000;
-  int* result = NULL;
-
-  printf("target: %d\n", target);
-  printf("input: ");
-  for (int i = 0; i < numsSize; ++i) {
-    printf("%d", nums[i]);
-    if (i != numsSize - 1)
-      printf(",");
-  }
-  printf("\n");
-  for (int i = 0; i < testCounts; ++i) {
-    funcTimer(&msec, &result, nums, numsSize, target, &returnSize, twoSum);
-    if (i == 0 && result != NULL)
-      printf("output: [%d,%d] ", result[0], result[1]);
-    //Why not we use free in here. To avoid the lib crt and main crt is different. Where is the var malloced, to free it in there.
-    safeFree(&result);
-  }
-  printf("and test %d spent time: %dms\n", testCounts, msec);
-  return 0;
+int main() {
+    clock_t init_time, end_time;
+    long time = 0;
+    int nums[] = {2, 7, 11, 15, 27, 30, 49, 81, 121, 143, 171, 197, 203, 239, 279};
+    int numsSize = sizeof(nums) / sizeof(nums[0]);
+    int target = 442;
+    int returnSize = 0;
+    int testCounts = 10000000;
+    int* result = NULL;
+  
+    printf("target: %d\n", target);
+    printf("input: ");
+    for (int i = 0; i < numsSize; ++i) {
+      printf("%d", nums[i]);
+      if (i != numsSize - 1)
+        printf(",");
+    }
+    printf("\n");
+    for (int i = 0; i < testCounts; ++i) {
+      funcTimer(&time, &result, nums, numsSize, target, &returnSize, twoSum);
+      if (i == 0 && result != NULL)
+        printf("output: [%d,%d] ", result[0], result[1]);
+      //Why not we use free in here. To avoid the lib crt and main crt is different. Where is the var malloced, to free it in there.
+      safeFree((void**)&result);
+    }
+    printf("and test %d spent time: %fms\n", testCounts, time / 1e6);
+    return 0;
 }
