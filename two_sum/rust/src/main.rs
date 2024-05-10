2890d8af4c4f15850f401b7456fa31f0c956921e
yilles <shuwei73.mail@gmail.com>
2024-05-10 18:06:16 +0800

    update submit2 and main

--- two_sum/rust/src/main.rs
+++ two_sum/rust/src/main.rs
@@ -1,17 +1,31 @@
 mod submit1;
 mod submit2;
-use submit1::two_sum;
+use std::time::Instant;
 
 fn main() {
-    let nums = vec![2, 7, 11, 15];
+    let nums = vec![2, 3, 32423, 234234, 5435, 565463, 5656352, 5563, 34435, 700, 435435, 7465654, 45453, 65465, 435435, 45345, 354435, 63634533, 330, 60, 3232, 3300, 32200, 40043, 4322330, 4030, 2302402, 232434, 343400, 3200, 8, 9, 10, 11, 15, 7];
     let target = 9;
-    let result = two_sum(&nums, target);
+
+    let start = Instant::now();
+    let mut result = None;
+    for _i in 0..100 {
+        result = submit1::two_sum(&nums, target);
+    }
+    let duration = start.elapsed();
+    println!("Time elapsed is: {:?}", duration);
     match result {
         Some((i, j)) => println!("{} + {} = {}", nums[i], nums[j], target),
         None => println!("No value"),
     }
-    let result2 = submit2::two_sum(&nums, target);
-    match result2 {
+
+    let start = Instant::now();
+    let mut result = None;
+    for _i in 0..100 {
+        result = submit2::two_sum(&nums, target);
+    }
+    let duration = start.elapsed();
+    println!("Time elapsed is: {:?}", duration);
+    match result {
         Some((i, j)) => println!("{} + {} = {}", nums[i], nums[j], target),
         None => println!("No value"),
     }
--- two_sum/rust/src/submit2.rs
+++ two_sum/rust/src/submit2.rs
@@ -1,3 +1,14 @@
+use std::collections::HashMap;
+
 pub fn two_sum(nums: &Vec<i32>, target: i32) -> Option<(usize, usize)> {
+    let mut map = HashMap::new();
+    for i in 0..nums.len() {
+        let complement = target - nums[i];
+        match map.get(&complement) {
+            Some(&j) => return Some((j, i)),
+            None => (),
+        }
+        map.insert(nums[i], i);
+    }
     None
 }
